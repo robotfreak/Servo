1f679db8b3dca2e9b3145192cdbc011c4d463982
RobotFreak <p.recktenwald@gmail.com>
2020-12-16 15:54:39 +0100

    Update Servo.cpp
    added support for all nRF51 nRF52 devices

--- src/nrf52/Servo.cpp
+++ src/nrf52/Servo.cpp
@@ -16,7 +16,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-#if defined(ARDUINO_ARCH_NRF52)
+#if defined(ARDUINO_ARCH_NRF52 || ARDUINO_ARCH_NRF5)
 
 #include <Arduino.h>
 #include <Servo.h>
